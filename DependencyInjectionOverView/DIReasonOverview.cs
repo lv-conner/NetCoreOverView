91f1a78b9ccfb66f7902eb06040f7c8af8dc5e9f
tim <1510899867@qq.com>
2018-04-24 23:56:25 +0800

    over view

---
+++ DependencyInjectionOverView/DIReasonOverview.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// 控制反转与依赖注入
+/// </summary>
+
+namespace DependencyInjectionOverView
+{
+    public class A
+    {
+        private B bInstance;
+
+        public A(B bInstance)
+        {
+            this.bInstance = bInstance;
+        }
+
+        public B BInstance { get => bInstance; set => bInstance = value; }
+    }
+
+    public class B
+    {
+        public B()
+        {
+
+        }
+    }
+}
